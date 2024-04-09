f7031649283985b3978026eda49b5c7fe2ca720e
Nagendra Gillalu <nagendragillalu@gmail.com>
2024-04-09 19:22:11 +0530

    Updated image

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://images.pexels.com/photos/4558740/pexels-photo-4558740.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         
