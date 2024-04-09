08e2fceed4a9df08d9d768fb60e610b06e4e0fcb
Nagendra Gillalu <nagendragillalu@gmail.com>
2024-04-09 18:44:01 +0530

    Update App.js for error fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br>
+        
             I am Learning Jenkins, Hello World!!
         </p>
         <a
